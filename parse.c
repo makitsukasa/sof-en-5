0ee11536d0e4c0453a94789fc768ed4f3be50827
tsukasa-boxy <allium.fistulosum.39+boxy@gmail.com>
2017-11-03 00:51:22 +0900

    hoge

--- parse.c
+++ parse.c
@@ -4,16 +4,27 @@
 #define PARSERESULT_EMPTY		2
 #define PARSERESULT_NOTMATCH	0
 
+#define DEBUG_PRINT_PARSER	0
+
 /* const */ SyntaxElem syntaxElems[NUMOFSYNTAX + 1];
 
 int token;
 
 int parse(int sElemIt, int depth){
-	int i, j;
+	int i;
 	SyntaxElem sElem = syntaxElems[sElemIt];
 
+#if DEBUG_PRINT_PARSER
+	int j;
 	for(i = 0; i < depth; i++) printf("\t");
 	printf("parse %s start\n", SYNTAXDIC[sElemIt]);
+	if(sElemIt > NUMOFTOKEN){
+		for(i = 0; i < depth; i++) printf("\t");
+		printf("<<< ");
+		for(i = 0; i < sElem.childrenNum; i++) printf("%s ", SYNTAXDIC[sElem.children[i]]);
+		printf(" >>>\n");
+	}
+#endif
 
 	switch(sElem.op){
 	int retVal;
@@ -21,18 +32,32 @@ int parse(int sElemIt, int depth){
 	/* check 1. to meet SINGLE TOKEN */
 	case SELEMOP_SINGLE_TOKEN:
 		if(sElemIt == SEMPTYSTAT){
+#if DEBUG_PRINT_PARSER
 			for(i = 0; i < depth; i++) printf("\t");
 			printf("parse %s end SINGLE EMPTY sElemIt:%d(%s)\n", SYNTAXDIC[sElemIt], token, SYNTAXDIC[token]);
+#endif
 			return PARSERESULT_EMPTY;
 		}
 		if(token == sElemIt){
+				if(sElemIt == TNAME || sElemIt == TNUMBER || sElemIt == TSTRING){
+#if DEBUG_PRINT_PARSER
+					for(i = 0; i < depth; i++) printf("\t");
+					printf("parse %s end SINGLE MATCH sElemIt:%d(%s), attr:%s\n", SYNTAXDIC[sElemIt], token, SYNTAXDIC[token], string_attr);
+#endif
+					token = scan();
+					return PARSERESULT_MATCH;
+				}
+#if DEBUG_PRINT_PARSER
 			for(i = 0; i < depth; i++) printf("\t");
 			printf("parse %s end SINGLE MATCH sElemIt:%d(%s)\n", SYNTAXDIC[sElemIt], token, SYNTAXDIC[token]);
+#endif
 			token = scan();
 			return PARSERESULT_MATCH;
 		}
+#if DEBUG_PRINT_PARSER
 		for(i = 0; i < depth; i++) printf("\t");
 		printf("parse %s end SINGLE NOTMATCH sElemIt:%d(%s)\n", SYNTAXDIC[sElemIt], token, SYNTAXDIC[token]);
+#endif
 		return PARSERESULT_NOTMATCH;
 
 	/* check 2. to meet ALL OF the conditions */
@@ -41,28 +66,34 @@ int parse(int sElemIt, int depth){
 		for(i = 0; i < sElem.childrenNum; i++){
 			switch(parse(sElem.children[i], depth + 1)){
 			case PARSERESULT_NOTMATCH:
+#if DEBUG_PRINT_PARSER
 				for(j = 0; j < depth; j++) printf("\t");
-				printf("parse %s end ALL NOTMATCH\n", SYNTAXDIC[sElemIt]);
+				printf("parse %s end ALL %s NOTMATCH\n", SYNTAXDIC[sElemIt], SYNTAXDIC[sElem.children[i]]);
+#endif
 				return PARSERESULT_NOTMATCH;
 
 			case PARSERESULT_EMPTY:
+#if DEBUG_PRINT_PARSER
 				for(j = 0; j < depth; j++) printf("\t");
-				printf("parse %s end ALL EMPTY ...\n", SYNTAXDIC[sElemIt]);
-				/******************************************************************************************/
-			
-			
-
-				/******************************************************************************************/
+				printf("parse %s end ALL %s EMPTY ...\n", SYNTAXDIC[sElemIt], SYNTAXDIC[sElem.children[i]]);
+#endif
 				break;
 
 			case PARSERESULT_MATCH:
 				retVal = PARSERESULT_MATCH;
+#if DEBUG_PRINT_PARSER
+				for(j = 0; j < depth; j++) printf("\t");
+				printf("parse %s end ALL %s MATCH ...\n", SYNTAXDIC[sElemIt], SYNTAXDIC[sElem.children[i]]);
+#endif
+				break;
 			}
 		}
+#if DEBUG_PRINT_PARSER
 		for(i = 0; i < depth; i++) printf("\t");
 		if(retVal == PARSERESULT_EMPTY)
 			printf("parse %s end ALL EMPTY\n", SYNTAXDIC[sElemIt]);
 		else printf("parse %s end ALL MATCH\n", SYNTAXDIC[sElemIt]);
+#endif
 		return retVal;
 
 	/* check 3. to meet ONE OF the conditions */
@@ -71,20 +102,26 @@ int parse(int sElemIt, int depth){
 		for(i = 0; i < sElem.childrenNum; i++){
 			switch(parse(sElem.children[i], depth + 1)){
 			case PARSERESULT_MATCH:
+#if DEBUG_PRINT_PARSER
 				for(j = 0; j < depth; j++) printf("\t");
 				printf("parse %s end ONE MATCH\n", SYNTAXDIC[sElemIt]);
+#endif
 				return PARSERESULT_MATCH;
 
 			case PARSERESULT_EMPTY:
+#if DEBUG_PRINT_PARSER
 				for(j = 0; j < depth; j++) printf("\t");
 				printf("parse %s end ONE EMPTY ...\n", SYNTAXDIC[sElemIt]);
+#endif
 				retVal = PARSERESULT_EMPTY;
 			}
 		}
+#if DEBUG_PRINT_PARSER
 		for(i = 0; i < depth; i++) printf("\t");
 		if(retVal == PARSERESULT_EMPTY)
 			printf("parse %s end ONE EMPTY\n", SYNTAXDIC[sElemIt]);
 		else printf("parse %s end ONE NOTMATCH\n", SYNTAXDIC[sElemIt]);
+#endif
 		return retVal;
 
 	/* check 4. to meet the condition ZERO OR MORE times */
@@ -93,10 +130,12 @@ int parse(int sElemIt, int depth){
 		while(parse(sElem.children[0], depth + 1)){
 			retVal = PARSERESULT_MATCH;
 		}
+#if DEBUG_PRINT_PARSER
 		for(i = 0; i < depth; i++) printf("\t");
 		if(retVal == PARSERESULT_EMPTY)
 			printf("parse %s end 0M EMPTY\n", SYNTAXDIC[sElemIt]);
 		else printf("parse %s end 0M MATCH\n", SYNTAXDIC[sElemIt]);
+#endif
 		return retVal;
 
 	/* check 5. to meet the condition ZERO OR ONE time */
@@ -104,10 +143,12 @@ int parse(int sElemIt, int depth){
 		if(parse(sElem.children[0], depth + 1) == PARSERESULT_MATCH)
 			retVal = PARSERESULT_MATCH;
 		else retVal = PARSERESULT_EMPTY;
+#if DEBUG_PRINT_PARSER
 		for(i = 0; i < depth; i++) printf("\t");
 		if(retVal == PARSERESULT_EMPTY)
 			printf("parse %s end 01 EMPTY\n", SYNTAXDIC[sElemIt]);
 		else printf("parse %s end 01 MATCH\n", SYNTAXDIC[sElemIt]);
+#endif
 		return retVal;
 
 	default:
